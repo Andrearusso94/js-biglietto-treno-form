e9690ac91ce3ccfd9025cb56af43d253ce5ea74c
Andrearusso94 <andrea.r.4845@gmail.com>
2022-10-07 16:01:36 +0200

    add calculator price

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-//km da percorrere
+/*//km da percorrere
 const nKm = Number < 100000;
 console.log(nKm);
 
@@ -18,7 +18,7 @@ console.log(price);
  
  
 
- /*//prezzi scontati
+ //prezzi scontati
 if (age < 18) {
  finalPrice = price - discountValueUnder;
  console.log(finalPrice)
@@ -29,23 +29,43 @@ if (age < 18) {
     finalPrice = price
     console.log("Non si applica nessuno sconto", finalPrice);
 }*/
-let numberKm = document.getElementById("nKm")
 
 
 
-function myFunction(){
-    let userKm = document.getElementById("nKm").value;
-    let age = document.getElementById("age").value;
-  
-    if (age < 18) {
-      finalPrice = price - discountValueUnder;
-      console.log(finalPrice)
-     } else if (age > 65) {
-        finalPrice = price - discountValueOver;
-        console.log(finalPrice)
-     } else  {
-         finalPrice = price
-         console.log("Non si applica nessuno sconto", finalPrice);
 
+const buttonGenerator = document.getElementById("go");
+
+buttonGenerator.addEventListener("click", function(){
+  const nKm = document.getElementById("nKm").value;
+
+
+//età viaggiatore
+const age = document.getElementById("age").value;
+
+
+
+// prezzo * Km
+const price = nKm * 0.21;
+
+
+// sconti  
+ let finalPrice;
+ const discountValueUnder = (price / 100)* 20;
+ const discountValueOver = (price/100)* 40;
+ 
+ 
+
+ //prezzi scontati
+if (age < 18) {
+ finalPrice = price - discountValueUnder;
+ console.log(finalPrice)
+} else if (age > 65) {
+   finalPrice = price - discountValueOver;
+   console.log(finalPrice)
+} else  {
+    finalPrice = price
+    console.log("Non si applica nessuno sconto", finalPrice);
 }
-}
+
+
+})
